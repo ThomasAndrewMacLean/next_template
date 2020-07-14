40b17568786e33550b3eed8c465396828436c087
ThomasAndrewMacLean <hello@thomasmaclean.be>
2020-07-14 22:21:07 +0200

    move to src folder

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -2,7 +2,7 @@ import React from 'react';
 import styled from 'styled-components';
 import PropTypes from 'prop-types';
 
-import { T } from '../components';
+import { T } from '.';
 
 const Layout = ({ children, page }) => {
   return (

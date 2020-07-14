235e45145abb534a2902f4958ecea2b958dfa8f6
ThomasAndrewMacLean <hello@thomasmaclean.be>
2020-07-14 23:34:02 +0200

    add prefix

--- next.config.js
+++ next.config.js
@@ -5,7 +5,7 @@ require('dotenv').config();
 const webpack = require('webpack');
 
 const isProduction = process.env.NODE_ENV === 'production';
-const prefix = isProduction ? '' : '';
+const prefix = isProduction ? '/next_template' : '';
 
 const envVariables = {
   AIRTABLE_APP: process.env.AIRTABLE_APP,

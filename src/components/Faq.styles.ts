5135e1e76e6b01b8efff1780541b198a84ce04eb
ThomasAndrewMacLean <hello@thomasmaclean.be>
2021-01-17 20:05:26 +0100

    slide in effect on faq

--- src/components/Faq.styles.ts
+++ src/components/Faq.styles.ts
@@ -1,5 +1,36 @@
 import styled from 'styled-components';
 
 export const FaqWrapper = styled.section`
- // background-color: var(--background-dark);
+  display: flex;
+  position: relative;
+  justify-content: space-between;
+  details {
+    max-width: 550px;
+    margin-bottom: 1rem;
+    font-weight: 200;
+  }
+  summary {
+    font-weight: 400;
+    cursor: pointer;
+    margin-bottom: 1rem;
+    outline: none;
+    &:focus {
+      font-weight: 800;
+    }
+  }
+
+  details[open] summary ~ * {
+    animation: sweep 0.5s ease-in-out;
+  }
+
+  @keyframes sweep {
+    0% {
+      opacity: 0;
+      transform: translateX(-10px);
+    }
+    100% {
+      opacity: 1;
+      transform: translateX(0px);
+    }
+  }
 `;

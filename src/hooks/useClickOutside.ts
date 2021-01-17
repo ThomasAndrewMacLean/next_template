a2b0f3199010e444208c1110d09601ba2cc378be
ThomasAndrewMacLean <hello@thomasmaclean.be>
2021-01-17 23:43:49 +0100

    add clickoutside hook

---
+++ src/hooks/useClickOutside.ts
@@ -0,0 +1,19 @@
+import { useEffect, MutableRefObject } from 'react';
+
+const useOutsideClick = (ref: MutableRefObject<any>, callback: () => {}) => {
+  const handleClick = (e: MouseEvent) => {
+    if (ref.current && !ref.current.contains(e.target)) {
+      callback();
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener('click', handleClick);
+
+    return () => {
+      document.removeEventListener('click', handleClick);
+    };
+  });
+};
+
+export default useOutsideClick;
